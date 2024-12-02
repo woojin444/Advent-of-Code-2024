7d462ecc06d61cf67449a1116adde9a0b85f3551
Woojin Ra <woojin444@gmail.com>
2024-12-02 12:16:04 +1100

    Adding comments

--- d1p1.py
+++ d1p1.py
@@ -1,23 +1,27 @@
-def insort(sorted_list, number):
-    low, high = 0, len(sorted_list)
+# Insort adds items into a sorted array without losing sort. Time complexity of O(log n)
+def insort(sorted_array, number):
+    low, high = 0, len(sorted_array)
     while low < high:
         mid = (low + high) // 2
-        if sorted_list[mid] < number:
+        if sorted_array[mid] < number:
             low = mid + 1
         else:
             high = mid
     
-    sorted_list.insert(low, number)
+    sorted_array.insert(low, number)
 
 list1 = []
 list2 = []
 
+# Goes through the list once and creates a sorted list
 with open("inputs/d1p12.txt", "r") as input_file:
     for line in input_file:
         number1, number2 = line.replace("\n","").split("   ")
         insort(list1, int(number1))
         insort(list2, int(number2))
 
+
+# Adds the differences
 difference = 0
 
 for i in range(len(list1)):
--- d1p2.py
+++ d1p2.py
@@ -1,6 +1,7 @@
 list1 = []
 list2 = {}
 
+# Goes through the lists and creates a dictionary of how many times a number is shown on the right side.
 with open("inputs/d1p12.txt", "r") as input_file:
     for line in input_file:
         number1, number2 = line.replace("\n","").split("   ")
@@ -10,6 +11,7 @@ with open("inputs/d1p12.txt", "r") as input_file:
         else:
             list2[int(number2)] = 1
 
+# Adds the similary scores
 similarity = 0
 
 for number in list1:
