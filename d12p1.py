d65dacffaba8c100a0f9c0fc8a35f73a3ea14c39
Woojin Ra <Woojin Ra>
2024-12-16 09:25:54 +1100

    d12p1

--- d12p1.py
+++ d12p1.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 # Data ingestion
 data = []
 with open("inputs/d12p12.txt") as input_file:
@@ -8,7 +10,26 @@ class Region:
     def __init__(self, letter: str):
         self.letter = letter
         self.coordinates = set()
-
+    
+    def populate(self, garden: list[list[Coordinate]], coordinate: Coordinate):
+        coordinate.assign(garden=garden, region=self)
+        coordinates_to_check = []
+        # If the next coordinate is valid
+        for next_coordinate in coordinate.next_coordinates(garden=garden):
+            # and if the next letter is the same letter as the region's letter,
+            # and if the next coordinate is not assigned.
+            if next_coordinate.letter == self.letter and not next_coordinate.is_assigned():
+                # then add the next coordinate to the list of coordinates to check.
+                coordinates_to_check.append(next_coordinate)
+        while len(coordinates_to_check) > 0:
+            coordinate_to_check = coordinates_to_check.pop()
+            coordinate_to_check.assign(garden=garden, region=self)
+            self.coordinates.add(coordinate_to_check)
+            for next_coordinate in coordinate_to_check.next_coordinates(garden=garden):
+                if next_coordinate not in coordinates_to_check and next_coordinate not in self.coordinates:
+                    if next_coordinate.letter == self.letter:
+                        coordinates_to_check.append(next_coordinate)
+            
 class Coordinate:
     def __init__(self, row: int, column: int, letter: str):
         self.letter = letter
@@ -17,7 +38,7 @@ class Coordinate:
         self.region = None
         self.perimeter = 0
 
-    def assign(self, garden: "list[list[Coordinate]]", region: Region):
+    def assign(self, garden: list[list[Coordinate]], region: Region):
         region.coordinates.add(self)
         self.region = region
         for direction in [(-1,0), (1,0), (0,-1), (0,1)]:
@@ -32,6 +53,16 @@ class Coordinate:
     def is_assigned(self):
         if self.region == None: return False
         else: return True
+
+    def next_coordinates(self, garden: list[list[Coordinate]]) -> list[Coordinate]:
+        valid_coordinates = []
+        for direction in [(-1,0), (1,0), (0,-1), (0,1)]:
+            next_row = self.row + direction[0]
+            next_column = self.column + direction[1]
+            # If the next coordinate is valid
+            if next_row >= 0 and next_row < len(garden) and next_column >= 0 and next_column < len(garden[next_row]):
+                valid_coordinates.append(garden[next_row][next_column])
+        return valid_coordinates
 
 # Garden creation with all the coordinates
 garden = []
@@ -48,6 +79,19 @@ regions = []
 row = 0
 column = 0
 
-while row < len(garden):
-    while column < len(garden[row]):
-        
+for row in garden:
+    for coordinate in row:
+        if not coordinate.is_assigned():
+            region = Region(coordinate.letter)
+            region.populate(garden=garden, coordinate=coordinate)
+            regions.append(region)
+
+answer = 0
+
+for region in regions:
+    area = len(region.coordinates)
+    perimeter = sum(map(lambda coordinate: coordinate.perimeter, region.coordinates))
+    print(f"Letter: {region.letter}. Area: {area}. Perimeter: {perimeter}")
+    answer += area * perimeter
+
+print(answer)
